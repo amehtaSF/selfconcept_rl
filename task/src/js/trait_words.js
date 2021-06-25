f602fa057c8e767ee286dda408acce826c40e79b
Ashish <ashm@stanford.edu>
2021-06-25 15:05:30 -0700

    Revert "added algoRating on finish to data"
    This reverts commit f50b6f26cbf1702c53954ea0d17484f19e9723e4.

--- task/src/js/trait_words.js
+++ task/src/js/trait_words.js
@@ -1,8 +1,6 @@
 var traits = [
     {word: "smart", valence: "positive"},
     {word: "funny", valence: "positive"},
-    {word: "cool", valence: "positive"},
     {word: "clumsy", valence: "negative"},
-    {word: "dumb", valence: "negative"},
-    {word: "boring", valence: "negative"}
+    {word: "boring", valence: "negative"},
 ]
