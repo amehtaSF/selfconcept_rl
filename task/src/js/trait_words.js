f50b6f26cbf1702c53954ea0d17484f19e9723e4
Ashish <ashm@stanford.edu>
2021-06-25 14:36:51 -0700

    added algoRating on finish to data

--- task/src/js/trait_words.js
+++ task/src/js/trait_words.js
@@ -1,6 +1,8 @@
 var traits = [
     {word: "smart", valence: "positive"},
     {word: "funny", valence: "positive"},
+    {word: "cool", valence: "positive"},
     {word: "clumsy", valence: "negative"},
-    {word: "boring", valence: "negative"},
+    {word: "dumb", valence: "negative"},
+    {word: "boring", valence: "negative"}
 ]
